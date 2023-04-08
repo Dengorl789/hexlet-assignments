0b217650a4726e88e0178056c5b3d13771ce65c5
@hexlet/cli <support@hexlet.io>
2023-04-09 00:06:51 +0300

    submit java-oop-ru/interfaces

---
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -0,0 +1,28 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+// BEGIN
+public class App {
+    public static List<String> buildApartmentsList(List<Home> apartments, int positions) {
+        if (apartments.isEmpty()) {
+            return new ArrayList<>();
+        }
+        List<Home> sortedApartmentsList = apartments.stream()
+                .sorted(Home::compareTo)
+                .toList();
+        List<String> sortedApartmentsListString = new ArrayList<>(positions);
+        for(int i = 0; i < positions; i++) {
+            var home = sortedApartmentsList.get(i);
+            sortedApartmentsListString.add(home.toString());
+        }
+        return sortedApartmentsListString;
+    }
+
+
+}
+// END
